2c9396e0e38b361a2d5301cf90f8498bdf2362f2
Oula Siljamo <osiljamo@gmail.com>
2020-11-10 13:45:28 +0200

    Implement some basic commands

--- controller.py
+++ controller.py
@@ -1,5 +1,5 @@
 import serial
-
+import time
 
 class Controller:
 
@@ -7,27 +7,70 @@ class Controller:
                  stop_bits=serial.STOPBITS_ONE, rtscts=True):
         """ Initialize the controller"""
         try:
-            self.serial_ = serial.Serial(device, baud_rate, timeout=5, parity=parity, rtscts=rtscts, stopbits=stop_bits,
+            self.serial_ = serial.Serial(device, baud_rate, timeout=10, parity=parity, rtscts=rtscts, stopbits=stop_bits,
                                          bytesize=data_bits)
         except serial.SerialException:
-            import os
-            print("Error connecting")
-            os.exit(0)
+            print("Error connecting to serial port")
+            exit(0)
 
     def __del__(self):
-        self.serial_.close()
+        try:
+            self.serial_.close()
+        except serial.SerialException:
+            print("Error closing serial port")
+            exit(0)
+
+    @staticmethod
+    def res_to_bool_(res):
+        return res == ">OK<"
+
+    @staticmethod
+    def to_bytes_(cmd):
+        return bytes(cmd, 'ascii')
+
+    def read_response_(self):
+        ser = self.serial_
+        res = ""
+        while True:
+            r = ser.read()
+            if r:
+                res += r.decode('ascii')
+                if "<" in res:
+                    break
+            else:
+                break
+        return res
+
+    # Common commands
 
     def set_current_range(self, current_range):
         """Sets the current range H for high (up to 100mA) and L for Low (up to 10mA)"""
-        pass
+        cmd = ">SR;{}".format(current_range)
+        self.serial_.write(self.to_bytes_(cmd))
+        res = self.read_response_()
+
+        return self.res_to_bool_(res)
 
     def set_voltage(self, voltage):
         """Sets voltage in volts (value between 70-150)"""
         pass
 
-    def set_pulse_generator(self, is_on):
-        """Set pulse generator on or off"""
-        pass
+    def set_pulse_generator(self, status: bool) -> bool:
+        """Set pulse DC/DC pulse generator on or off
+
+        :param status: Status of pulse generator
+
+        :return: True if setting pulse generator succeeded.
+        """
+        if status:
+            cmd = ">ON<"
+        else:
+            cmd = ">OFF<"
+
+        self.serial_.write(self.to_bytes_(cmd))
+        res = self.read_response_()
+
+        return self.res_to_bool_(res)
 
     def set_num_nplets(self, num):
         """Set the number of n-plets to be generated"""
@@ -45,9 +88,15 @@ class Controller:
         """Sets pulse generators either active or not active"""
         pass
 
-    def read_batter(self):
+    def read_battery(self):
         """Read remaining battery capacity"""
-        pass
+        cmd = ">SOC<"
+        cmd = bytes(cmd, 'utf-8')
+        self.serial_.write(cmd)
+
+        res = self.read_response_()
+        battery_level = int.from_bytes(bytes(res[-2], 'ascii'), byteorder="big")
+        return battery_level
 
     # long protocol
 
