554b9082ccf9a79af9273d666771df3df5320198
Oula Siljamo <osiljamo@gmail.com>
2020-11-18 12:36:09 +0200

    Add base for text interface for the controller

---
+++ TUI.py
@@ -0,0 +1,203 @@
+from controller import Controller
+
+import py_cui
+
+import logging
+
+class TUI:
+    def __init__(self, device: Controller):
+
+        logging.basicConfig(filename="ui_log.log", level=logging.DEBUG)
+
+        self.labels = [
+            "Current range: {}, Voltage: {}, Mode: {}"
+        ]
+        self.device = device
+        self.master = py_cui.PyCUI(30, 10)
+
+        span = 10
+
+        self.stats = self.master.add_block_label(self.labels[0].format(
+            device.current_range, device.voltage, device.mode),
+            0, 0, column_span=span, center=False)
+        self.converter = self.master.add_block_label("DC/DC Converter: {}".format(
+            "On" if device.pulse_generator_dc_converter_status else "Off"),
+            1, 0, column_span=span, center=False)
+        self.master.add_block_label("Pulse generation: {}".format(
+            "On" if device.pulse_generator_dc_converter_status else "Off"),
+            2, 0, column_span=span, center=False)
+        self.master.add_block_label("Number of n-plets: {}".format(
+            device.num_nplets if device.num_nplets != 0 else "0 (Infinite)"),
+            3, 0, column_span=span, center=False)
+        self.master.add_block_label("Time between pulses: {}ms".format(device.time_between),
+                             4, 0, column_span=span, center=False)
+        self.master.add_block_label("N-plet repetition rate: {}pps".format(device.repetition_rate),
+                             5, 0, column_span=span, center=False)
+        self.master.add_block_label("Delay after trigger: {}ms".format(device.delay),
+                             6, 0, column_span=span, center=False)
+        self.master.add_block_label("Pulse widths: {} (unit: μs)".format(device.pulse_widths),
+                             7, 0, column_span=span, center=False)
+        self.master.add_block_label("Pulse amplitudes: {} (unit: mA)".format(
+            self._calculate_amplitudes(device.pulse_amplitudes, device.mode)),
+            8, 0, column_span=span, center=False)
+        self.master.add_block_label("Output channels (unipolar): {}".format(device.output_channels),
+                             9, 0, column_span=span, center=False)
+        self.master.add_block_label("Channel pairs (bipolar): {}".format(device.channel_pairs),
+                             10, 0, column_span=span, center=False)
+        self.battery = self.master.add_block_label("Battery: {}%".format(device.battery_state), 11, 0, column_span=span,
+                                       center=False)
+
+        self.command_prompt = self.master.add_text_box("Command: ", 29, 0, column_span=10)
+        command_list1 = self.master.add_scroll_menu("Commands", 12, 0, row_span=9, column_span=5)
+        commands1 = [
+            "battery: Update battery information",
+            "mode <mode>: Set stimulatio mode (unipolar or bipolar)",
+            "range <range>: ",
+            "voltage <voltage>: Set voltage (70-150)"
+        ]
+        command_list1.add_item_list(commands1)
+        command_list2 = self.master.add_scroll_menu("Commands", 12, 5, row_span=9, column_span=5)
+        self.command_history = self.master.add_scroll_menu("Command history", 21, 0, row_span=8, column_span=10)
+
+        self.command_prompt.add_key_command(py_cui.keys.KEY_ENTER, self.send_command)
+
+        self.master.start()
+
+    @staticmethod
+    def _calculate_amplitudes(amplitudes: list, mode='low') -> list:
+        if mode == 'low':
+            div = 100
+        elif mode == 'high':
+            div = 10
+        else:
+            return amplitudes
+
+        new = []
+        for i in amplitudes:
+            new.append(i/div)
+        return new
+
+    def _parse_input(self, text: str) -> str:
+        out = text.lower()
+        parts = text.split()
+        cmd = parts[0]
+        if cmd == 'battery':
+            self.device.read_battery()
+            self.battery.set_title("Battery: {}%".format(self.device.battery_state))
+        elif cmd == 'mode':
+            try:
+                mode = parts[1]
+                succ = self.device.set_mode(mode)
+                if succ:
+                    self.stats.set_title(self.labels[0].format(
+                        self.device.current_range, self.device.voltage, self.device.mode))
+                else:
+                    out = "Command: {}, failed for unknown reason"
+            except ValueError as e:
+                logging.debug(e)
+                out = "Error: {}".format(e)
+            except IndexError:
+                out = "Mode command requires one parameter"
+        elif cmd == 'range':
+            try:
+                current_range = parts[1]
+                succ = self.device.set_current_range(current_range)
+                if succ:
+                    self.stats.set_title(self.labels[0].format(
+                        self.device.current_range, self.device.voltage, self.device.mode))
+            except ValueError as e:
+                logging.debug(e)
+                out = "Error: {}".format(e)
+            except IndexError:
+                out = "Range command requires one parameter"
+        elif cmd == 'voltage':
+            try:
+                voltage = parts[1]
+                succ = self.device.set_voltage(int(voltage))
+                if succ:
+                    self.stats.set_title(self.labels[0].format(
+                        self.device.current_range, self.device.voltage, self.device.mode))
+            except ValueError as e:
+                logging.debug(e)
+                out = "Error: {}".format(e)
+            except IndexError:
+                out = "Voltage command requires one parameter"
+        else:
+            out = "Command not found: {}".format(out)
+
+        return out
+
+    def send_command(self):
+        text = self.command_prompt.get()
+        out = self._parse_input(text)
+        self.command_prompt.clear()
+        self.command_history.add_item(out)
+
+
+def tui(device: Controller):
+    root = py_cui.PyCUI(30, 10)
+    root.set_title("Bimatrix controller")
+    root.set_refresh_timeout(1)
+    info_list = [
+        "Current range: {}, Voltage: {}, Mode: {}".format(
+            device.current_range, device.voltage, device.mode),
+        "DC/DC Converter: {}".format(
+            "On" if device.pulse_generator_dc_converter_status else "Off"),
+        "Pulse generation: {}".format(
+            "On" if device.pulse_generator_dc_converter_status else "Off"),
+        "Number of n-plets: {}".format(
+            device.num_nplets if device.num_nplets != 0 else "0 (Infinite)"),
+        "Time between pulses: {}ms".format(device.time_between),
+        "N-plet repetition rate: {}pps".format(device.repetition_rate),
+        "Delay after trigger: {}ms".format(device.delay),
+        "Pulse widths: {} (unit: μs)".format(device.pulse_widths),
+        "Pulse amplitudes: {} (unit: mA)".format(
+            calculate_amplitudes(device.pulse_amplitudes, device.mode)),
+        "Output channels (unipolar): {}".format(device.output_channels),
+        "Channel pairs (bipolar): {}".format(device.channel_pairs),
+    ]
+    #info = root.add_scroll_menu("Device info", 0, 0, row_span=5, column_span=10)
+    #info.add_item_list(info_list)
+    #info.set_selectable(False)
+
+    info_list.append("Test elements")
+
+    """span = 10
+    curr_range =root.add_block_label("Current range: {}, Voltage: {}, Mode: {}".format(
+        device.current_range, device.voltage, device.mode),
+        0, 0, column_span=span, center=False)
+    converter = root.add_block_label("DC/DC Converter: {}".format(
+        "On" if device.pulse_generator_dc_converter_status else "Off"),
+        1, 0, column_span=span, center=False)
+    root.add_block_label("Pulse generation: {}".format(
+        "On" if device.pulse_generator_dc_converter_status else "Off"),
+        2, 0, column_span=span, center=False)
+    root.add_block_label("Number of n-plets: {}".format(
+        device.num_nplets if device.num_nplets != 0 else "0 (Infinite)"),
+        3, 0, column_span=span, center=False)
+    root.add_block_label("Time between pulses: {}ms".format(device.time_between),
+                         4, 0, column_span=span, center=False)
+    root.add_block_label("N-plet repetition rate: {}pps".format(device.repetition_rate),
+                         5, 0, column_span=span, center=False)
+    root.add_block_label("Delay after trigger: {}ms".format(device.delay),
+                         6, 0, column_span=span, center=False)
+    root.add_block_label("Pulse widths: {} (unit: μs)".format(device.pulse_widths),
+                         7, 0, column_span=span, center=False)
+    root.add_block_label("Pulse amplitudes: {} (unit: mA)".format(
+        calculate_amplitudes(device.pulse_amplitudes, device.mode)),
+                         8, 0, column_span=span, center=False)
+    root.add_block_label("Output channels (unipolar): {}".format(device.output_channels),
+                         9, 0, column_span=span, center=False)
+    root.add_block_label("Channel pairs (bipolar): {}".format(device.channel_pairs),
+                         10, 0, column_span=span, center=False)
+    battery = root.add_block_label("Battery: {}%". format(device.battery_state), 11, 0, column_span=span, center=False)
+
+    command_list1 = root.add_scroll_menu("Commands", 12, 0, row_span=9, column_span=5)
+    command_list2 = root.add_scroll_menu("Commands", 12, 5, row_span=9, column_span=5)
+    command_history = root.add_scroll_menu("Command history", 21, 0, row_span=8, column_span=span)
+    commands = root.add_text_box("Command: ", 29, 0, column_span=10)"""
+
+    # Keys
+    root.start()
+
+    device.read_battery()
--- main.py
+++ main.py
@@ -1,10 +1,9 @@
-import time
-
 import serial
 import argparse
 import logging
 
-import controller
+from controller import Controller
+from TUI import TUI
 
 BAUD_RATE = 921600
 DATA_BITS = serial.EIGHTBITS
@@ -14,211 +13,22 @@ RTSCTS = True
 
 
 def log_level(x: str):
-    levels = {
+    return {
         'debug': logging.DEBUG,
         'info': logging.INFO,
         'warning': logging.WARNING,
         'error': logging.ERROR,
         'critical': logging.CRITICAL
-    }
-    return levels[x.lower()]
+    }.get(x.lower(), logging.error)
 
 
-def commadline_mode(arg):
-    with serial.Serial(arg.device, BAUD_RATE, timeout=None, parity=PARITY, rtscts=RTSCTS, stopbits=STOP_BITS,
-                       bytesize=DATA_BITS) as ser:
-        while True:
-            c = input("Command: ")
-            if c.lower() == "quit":
-                break
-            s = '>%s<' % c
-            print("Sending command {}".format(c))
-            ser.write(bytes(s, 'utf-8'))
-            res = ""
-            while True:
-                r = ser.read()
-                if r:
-                    res += r.decode('ascii')
-                    if "<" in res:
-                        print(res)
-                        break
-                else:
-                    print("Pass")
-
-def predefined_mode(arg):
-    # Example commands
-    commands = ['ON', 'SV;0x78', 'MUX;ON', 'SF;0x0032', 'SR;H',
-                'CA;0x200000 0x400000 0x000001 0x000002 0x004000 0x008000 0x000000 0x000000 0x000000 0x000000',
-                'CA;0x200000 0x400000 0x000001 0x000002 0x004000 0x008000 0x000000 0x000000 0x000000 0x000000',
-                'PW;0x00FA 0x00FA 0x00FA 0x00FA 0x00FA 0x00FA', 'T', 'T', 'OFF', 'SOC']
-    with serial.Serial(arg.device, BAUD_RATE, timeout=None, parity=PARITY, rtscts=RTSCTS, stopbits=STOP_BITS, bytesize=DATA_BITS) as ser:
-        """for c in commands:
-            s = '>%s<' % c
-            print("Sending command {}".format(c))
-            ser.write(bytes(s, 'utf-8'))
-            res = ""
-            while True:
-                r = ser.read()
-                if r:
-                    res += r.decode('ascii')
-                    if "<" in res:
-                        print(res)
-                        break
-                else:
-                    print("Pass")"""
 def main(args):
     print("Starting...")
+    device = Controller(args.device, logging_level=log_level(args.logging_level))
+    print(device)
+    print("Started")
 
-    device = controller.Controller(args.device, logging_level=log_level(args.logging_level))
-    commands = ['ON', 'SV;0x78', 'MUX;OFF', 'SYNC;A', 'SR;H', 'MP;0x000015 0x32',
-                'SC;0x0064 0x0000 0x00C8 0x0000 0x01F4 0x0000 0x0000',
-                'PW;0x00FA 0x00FA 0x00FA 0x00FA 0x00FA 0x00FA', 'T', 'OFF', 'SOC']
-    print("Started.")
-
-    #for c in commands:
-        #print(bytes(c, 'utf-8'))
-    #commadline_mode(arg)
-    #predefined_mode(arg)
-
-    res = device.set_pulse_generator(True)
-    print(res)
-    battery_level = device.read_battery()
-    print("Battery level is: {}%".format(battery_level))
-    res = device.set_pulse_generator(False)
-    print(res)
-
-    print("Test current range")
-    res = device.set_current_range('H')
-    print(res)
-    res = device.set_current_range('L')
-    print(res)
-
-    print("Test voltage")
-    res = device.set_voltage(150)
-    print(res)
-    res = device.set_voltage(70)
-    print(res)
-    res = device.set_voltage(85)
-    print(res)
-
-    print("Test num of n-plets")
-    res = device.set_num_nplets(0)
-    print(res)
-    res = device.set_num_nplets(16777215)
-    print(res)
-    res = device.set_num_nplets(14500)
-    print(res)
-
-    print("Test time between pulses")
-    res = device.set_time_between(255)
-    print(res)
-    res = device.set_time_between(1)
-    print(res)
-    res = device.set_time_between(126)
-    print(res)
-
-    print("Test delay")
-    res = device.set_delay(0)
-    print(res)
-    res = device.set_delay(16777215)
-    print(res)
-    res = device.set_delay(5000)
-    print(res)
-
-    print("Test trigger")
-
-    res = device.trigger_pulse_generator()
-    print(res)
-
-    print("Sleep for 5 seconds")
-    #time.sleep(5)
-
-    res = device.trigger_pulse_generator()
-    print(res)
-
-    '''print("Test n-plet repetition rate")
-    res = device.set_repetition_rate(1)
-    print(res)
-    res = device.set_repetition_rate(400)
-    print(res)
-
-    print("Test pulse widths")
-    res = device.set_pulse_width([50, 50, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0])
-    print(res)
-
-    print("Test amplitude")
-    res = device.set_amplitude([50, 50, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0])
-    print(res)
-
-    print("Test mode")
-    res = device.set_mode('unipolar')
-    print(res)
-
-    print("Test common electrode")
-    res = device.set_common_electrode('A')
-    print(res)
-    res = device.set_common_electrode('C')
-    print(res)
-
-    print("Test unipolar channels")
-    res = device.set_pulses_unipolar(
-        ['800000', '400000', '200000', '100000', '080000', '040000', '020000', '010000',
-         '008000', '004000', '002000', '001000', '000800', '000400', '000200', '000100',
-         '000000', '000000', '000000', '000000', '000000', '000000', '000000', '000000'], value_type='hex')
-    print(res)
-    """res = device.set_pulses_unipolar(
-        [
-            [24], [23], [22], [21], [20], [19], [18], [17], [16], [15], [14],
-            [13], [12], [11], [10], [9], [8], [7], [6], [5], [4], [3], [2], [1]
-        ], value_type='list'
-    )"""
-    res = device.set_pulses_unipolar(
-        [
-            [24], [23], [0], [0], [0], [0], [0], [0], [0], [0], [0],
-            [0], [0], [0], [0], [0], [0], [0], [0], [0], [0], [0], [0], [0]
-        ]
-    )
-    print(res)
-
-    print("Change to bipolar")
-    res = device.set_mode('bipolar')
-    print(res)
-
-    print("Test bipolar")
-    res = device.set_pulses_bipolar(
-        [('200000', '400000'), ('000001', '000002'), ('004000', '008000'), ('000000', '000000'), ('000000', '000000'),
-         ('000000', '000000'), ('000000', '000000'), ('000000', '000000'), ('000000', '000000'), ('000000', '000000'),
-         ('000000', '000000'), ('000000', '000000'), ('000000', '000000'), ('000000', '000000'), ('000000', '000000'),
-         ('000000', '000000'), ('000000', '000000'), ('000000', '000000'), ('000000', '000000'), ('000000', '000000'),
-         ('000000', '000000'), ('000000', '000000'), ('000000', '000000'), ('000000', '000000')], value_type='hex'
-    )
-    print(res)
-    res = device.set_pulses_bipolar(
-        [([22], [23]), ([1], [2]), ([15], [16]), ([0], [0]), ([0], [0]), ([0], [0]), ([0], [0]), ([0], [0]), ([0], [0]),
-         ([0], [0]), ([0], [0]), ([0], [0]), ([0], [0]), ([0], [0]), ([0], [0]), ([0], [0]), ([0], [0]), ([0], [0]),
-         ([0], [0]), ([0], [0]), ([0], [0]), ([0], [0]), ([0], [0]), ([0], [0])]
-    )
-    print(res)'''
-
-    print("Set unipolar")
-    res = device.set_mode('unipolar')
-    print(res)
-
-    print("Test short cathode&anode")
-    res = device.set_common_electrode_short('C')
-    print(res)
-    res = device.set_common_electrode_short('A')
-    print(res)
-
-    print("Test short output activity")
-    res = device.set_output_channel_activity('000015', 50, value_type='hex')
-    print(res)
-    res = device.set_output_channel_activity([1,3,5], 50)
-    print(res)
-
-    device.close_serial()
-
-    print("Quitting...")
+    TUI(device)
 
 
 if __name__ == '__main__':
