da87d9e80b674ba4de8c3810a822173701fc2630
Oula Siljamo <osiljamo@gmail.com>
2020-11-19 23:59:15 +0200

    Add defaults to program arguments

--- main.py
+++ main.py
@@ -35,8 +35,8 @@ def main(args):
 if __name__ == '__main__':
     parser = argparse.ArgumentParser(description='Controller options')
     parser.add_argument('-d', '--device', help='Device serial port')
-    parser.add_argument('-l', '--logging_level', help='Logging level')
-    parser.add_argument('-f', '--log_file', help='Log file')
-    parser.add_argument('-c', '--commands', help='Commands to be executed on the controller')
+    parser.add_argument('-l', '--logging_level', default="warning", help='Logging level')
+    parser.add_argument('-f', '--log_file', default="", help='Log file')
+    parser.add_argument('-c', '--commands', default="", help='Commands to be executed on the controller')
     arguments = parser.parse_args()
     main(arguments)
