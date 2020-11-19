4891cc03e3e62f2d49415505def6094b40c26b9d
Oula Siljamo <osiljamo@gmail.com>
2020-11-19 23:28:24 +0200

    Add parameter for log file and custom commands

--- main.py
+++ main.py
@@ -24,16 +24,18 @@ def log_level(x: str):
 
 def main(args):
     print("Starting...")
-    device = Controller(args.device, logging_level=log_level(args.logging_level))
+    device = Controller(args.device, logging_level=log_level(args.logging_level), log_file=args.log_file)
     print(device)
     print("Started")
 
-    TUI(device)
+    TUI(device, config_file=args.commands)
 
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser(description='Controller options')
     parser.add_argument('-d', '--device', help='Device serial port')
     parser.add_argument('-l', '--logging_level', help='Logging level')
+    parser.add_argument('-f', '--log_file', help='Log file')
+    parser.add_argument('-c', '--commands', help='Commands to be executed on the controller')
     arguments = parser.parse_args()
     main(arguments)
