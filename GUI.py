e9b63922a24cd6d6bfd5647f5c7af3c28fc8db09
stas <stas>
2022-04-12 09:30:55 +0300

    GUI improvements: added cathode/anode for electrode selection, threading for sweep

--- GUI.py
+++ GUI.py
@@ -2,7 +2,7 @@ from PyQt6.QtWidgets import QApplication, QWidget, QMainWindow, QGraphicsView, Q
      QPushButton, QHBoxLayout, QVBoxLayout, QTableView, QDialog, QLabel, QLineEdit, QGraphicsScene, QGraphicsSimpleTextItem, QGraphicsTextItem, \
      QTabWidget, QFormLayout
 from PyQt6.QtGui import QPen, QColor
-from PyQt6.QtCore import Qt, QRectF, QEventLoop, QEvent, QObject, QThread
+from PyQt6.QtCore import Qt, QRectF, QEventLoop, QEvent, QObject, QThread, pyqtSignal
 import sys
 from controller import Controller
 import time
@@ -14,6 +14,24 @@ class ExitButton(QPushButton):
     def __init__(self):
         super().__init__("Exit")
         self.clicked.connect(sys.exit)
+    
+class Worker(QObject):
+    finished = pyqtSignal()
+    progress = pyqtSignal(list)
+
+    def __init__(self, pairs, between):
+        """
+        QWorker for sweeping without logging, GUI hangs if this is done w/o a thread
+        """
+        super().__init__()
+        self.pairs = pairs
+        self.between = between
+
+    def run(self):
+        for pair in self.pairs:
+            self.progress.emit(pair)
+            time.sleep(self.between)
+        self.finished.emit()
 
 class Channel(QGraphicsEllipseItem):
     def __init__(self, y, x, sz, channel_n):
@@ -24,20 +42,26 @@ class Channel(QGraphicsEllipseItem):
         self.y = 15+y*sz
  
         self.setRect(self.x, self.y, self.rectsize, self.rectsize)
-        self.setBrush(QColor(0, 0, 255))
+        self.setBrush(QColor(238, 238, 238))
         self.show()
 
-        self.on = False
+        self.mode = "off"
 
     def mousePressEvent(self, *args, **kwargs):
-        if not self.on:
-            self.setBrush(QColor(255, 0, 0))
-            self.on = True
-            self.scene().views()[0].active_channels.append(self.channel_n)
-        else:
+        if self.mode == "off":
             self.setBrush(QColor(0, 0, 255))
-            self.on = False
-            self.scene().views()[0].active_channels.remove(self.channel_n)
+            self.mode = "cathode"
+            self.scene().views()[0].active_cathodes.append(self.channel_n)
+        elif self.mode == "cathode":
+            self.setBrush(QColor(255, 0, 0))
+            self.mode = "anode"
+            self.scene().views()[0].active_cathodes.remove(self.channel_n)
+            self.scene().views()[0].active_anodes.append(self.channel_n)
+        else:
+            self.setBrush(QColor(238, 238, 238))
+            self.mode = "off"
+            self.scene().views()[0].active_anodes.remove(self.channel_n)
+            
 
 class ChannelView(QGraphicsView):
     def __init__(self):
@@ -47,8 +71,23 @@ class ChannelView(QGraphicsView):
         self.setScene(self.scene)
         self.sz = 100
         self.add_channels()
-        self.active_channels = []
-
+        self.active_cathodes = []
+        self.active_anodes = []
+        
+        # Show labels
+        cat = QGraphicsEllipseItem(0,0,20,20)
+        cat.setBrush(QColor(0,0,255))
+        cat_label = QGraphicsTextItem(f"Cathode")
+        cat_label.setPos(25,-5)
+        self.scene.addItem(cat_label)
+        self.scene.addItem(cat)
+        
+        ano = QGraphicsEllipseItem(0,-25,20,20)
+        ano.setBrush(QColor(255,0,0))
+        ano_label = QGraphicsTextItem(f"Anode")
+        ano_label.setPos(25,-25)
+        self.scene.addItem(ano_label)
+        self.scene.addItem(ano)
 
     def add_channels(self):
         for y in range(4):
@@ -60,7 +99,7 @@ class ChannelView(QGraphicsView):
                 self.scene.addItem(label)
 
     def get_active_channels(self):
-        return self.active_channels
+        return self.active_cathodes, self.active_anodes
 
 class ChannelSwipe(QWidget):
     def __init__(self, channels, device):
@@ -74,6 +113,7 @@ class ChannelSwipe(QWidget):
         self.widths = QLineEdit("1000")
         self.freq = QLineEdit("50")
         self.between = QLineEdit("0.5")
+        #self.between = QLineEdit()
         self.layout.addRow("Voltage (V)", self.voltage)
         self.layout.addRow("Pulse repetitions", self.num_nplets)
         self.layout.addRow("Amplitude (mA)", self.amplitudes)
@@ -94,6 +134,10 @@ class ChannelSwipe(QWidget):
         self.stim_status = QLabel("")
         self.layout.addWidget(self.stim_status)
 
+        self.stop_stim = QPushButton("Stop stimulation")
+        self.stop_stim.clicked.connect(lambda: abort_serial(self.device))
+        self.layout.addWidget(self.stop_stim)
+
         self.setLayout(self.layout)
 
         self.channels = channels
@@ -111,31 +155,54 @@ class ChannelSwipe(QWidget):
             self.settings_status.setText("Settings failed")
         else:
             self.settings_status.setText("Settings OK")
+    
+    def single_stim(self, pair):
+        """
+        perform single stimulation when QWorker emits a signal
+        """
+        self.stim_status.setText(f"Currently at {pair}")
+        res = True
+        res = self.device.set_pulses_bipolar(pair)
+        res = self.device.trigger_pulse_generator()
+        if not res:
+            self.stim_status.setText(f"Stimulation failed at {pair}")
 
     def trigger_sweep(self):
         """
         Loops over all selected electrode pairs (both anode and cathode is tried for one pair)
-        If self.between is selected, just does the run
+        If self.between is selected, just does the run in a thread so GUI doesn't hang
         If self.between is empty, prompts user for key press to record stimulation result, finally saving res to file
         """
         pairs = self.generate_combinations()
         if not pairs:
-            raise Expection("Please select at least one electrode pair")
-        for pair in pairs:
-            self.electrodes = pair
-            res = True
-            res = self.device.set_pulses_bipolar(pair)
-            res = self.device.trigger_pulse_generator()
-            if not res:
-                raise Exception("Device trigger failed")
-            if self.between.text():
-                time.sleep(float(self.between.text()))
-            else:
-                self.stim_status.setText(f"Currently at {pair}")
-                self.loop = QEventLoop()
-                self.loop.exec()
-        self.loop = None
-        self.stim_status.setText(f"Done")
+            raise Exception("Please select at least one electrode pair")
+
+        if not self.between.text():
+            self.thread = QThread()
+            self.worker = Worker(pairs, float(self.between.text()))
+            self.worker.moveToThread(self.thread)
+            self.thread.started.connect(self.worker.run)
+            self.worker.finished.connect(self.thread.quit)
+            self.worker.finished.connect(self.worker.deleteLater)
+            self.thread.finished.connect(self.thread.deleteLater)
+            self.worker.progress.connect(self.single_stim)
+            self.thread.start()
+    
+        else:
+            for pair in pairs:
+                self.electrodes = pair
+                res = True
+                res = self.device.set_pulses_bipolar(pair)
+                res = self.device.trigger_pulse_generator()
+                if not res:
+                    raise Exception("Device trigger failed")
+                else:
+                    self.stim_status.setText(f"Currently at {pair}")
+                    self.loop = QEventLoop()
+                    self.loop.exec()
+                self.loop = None
+            self.stim_status.setText(f"Done")
+
         if not self.between.text():
             with open(f"{datetime.now().isoformat()}.csv", "w") as file:
                 file.write("voltage,nplets,freq,width,amplitude,cathode,anode,result\n")
@@ -143,12 +210,14 @@ class ChannelSwipe(QWidget):
                 self.tofile = ""
 
     def generate_combinations(self):
+        """
+        Generates electrode pairs for selected cathodes and anodes
+        """
         pairs = []
-        actives = self.channels.get_active_channels()
-        for i in range(len(actives)):
-            for j in range(len(actives)):
-                if i != j:
-                    pairs.append(([actives[i],actives[j]]))
+        cathodes, anodes = self.channels.get_active_channels()
+        for i in range(len(cathodes)):
+            for j in range(len(anodes)):
+                pairs.append(([cathodes[i],anodes[j]]))
         return pairs
 
     def keyPressEvent(self, event):
@@ -212,17 +281,17 @@ class AmplitudeSwipe(QWidget):
         res = self.device.set_repetition_rate(int(self.freq.text()))
         res = self.device.set_pulse_width([int(self.widths.text())])
         # first element of active channels cathode, second anode
-        electrodes = self.channels.get_active_channels()
-        self.electrodes = electrodes
-        if len(electrodes) < 2:
-            # Tähän throwin sijaan jotain järkevää
-            raise Exception("Please select two electrodes")
-        electrodes = [(electrodes[0], electrodes[1])]
-        res = self.device.set_pulses_bipolar(electrodes)
-        if not res:
-            self.settings_status.setText("Settings failed")
-        else:
-            self.settings_status.setText("Settings OK")
+        cathodes, anodes = self.channels.get_active_channels()
+        try:
+            self.electrodes = [cathodes[0], anodes[0]]
+            electrodes = [(self.electrodes[0], self.electrodes[1])]
+            res = self.device.set_pulses_bipolar(electrodes)
+            if not res:
+                self.settings_status.setText("Settings failed")
+            else:
+                self.settings_status.setText("Settings OK")
+        except:
+            self.stim_status.setText("Please select a cathode and an anode")
 
     def trigger_sweep(self):
         """
@@ -301,6 +370,7 @@ class FrequencySwipe(QWidget):
         self.stim_status = QLabel("")
         self.layout.addWidget(self.stim_status)
 
+
         self.setLayout(self.layout)
 
         self.channels = channels
@@ -313,16 +383,17 @@ class FrequencySwipe(QWidget):
         res = self.device.set_num_nplets(int(self.num_nplets.text()))
         res = self.device.amplitude(int(self.amplitudes.text()))
         res = self.device.set_pulse_width([int(self.widths.text())])
-        electrodes = self.channels.get_active_channels()
-        self.electrodes = electrodes
-        if len(electrodes) < 2:
-            raise Exception("Please select two electrodes")
-        electrodes = [(electrodes[0], electrodes[1])]
-        res = self.device.set_pulses_bipolar(electrodes)
-        if not res:
-            self.settings_status.setText("Settings failed")
-        else:
-            self.settings_status.setText("Settings OK")
+        cathodes, anodes = self.channels.get_active_channels()
+        try:
+            self.electrodes = [cathodes[0], anodes[0]]
+            electrodes = [(self.electrodes[0], self.electrodes[1])]
+            res = self.device.set_pulses_bipolar(electrodes)
+            if not res:
+                self.settings_status.setText("Settings failed")
+            else:
+                self.settings_status.setText("Settings OK")
+        except:
+            self.stim_status.setText("Please select a cathode and an anode")
 
     def trigger_sweep(self):
         self.current_freq = int(self.start.text())
@@ -383,6 +454,7 @@ class MainWindow(QWidget):
         self.setLayout(self.horizontal_layout)
         self.show()
 
+
     def set_settings(self):
         self.step = QLineEdit()
         self.vertical_layout.addWidget(self.step)
@@ -395,9 +467,9 @@ class MainWindow(QWidget):
         self.tab1 = ChannelSwipe(self.channels, self.device) 
         self.tab2 = AmplitudeSwipe(self.channels, self.device)
         self.tab3 = FrequencySwipe(self.channels, self.device)
+        self.tabs.addTab(self.tab1,"Swipe channels")
         self.tabs.addTab(self.tab2,"Swipe amplitudes")
         self.tabs.addTab(self.tab3,"Swipe frequencies")
-        self.tabs.addTab(self.tab1,"Swipe channels")
         
     def create_menu(self):
         self.menubar = QVBoxLayout()
@@ -411,6 +483,10 @@ class MainWindow(QWidget):
 
         self.statistics = QLabel("")
         self.menubar.addWidget(self.statistics)
+        # refaktorointi tulee olemaan 5/5 kokemus XD osaispa koodaa
+        self.tab1.apply.clicked.connect(self.get_current_settings)
+        self.tab2.apply.clicked.connect(self.get_current_settings)
+        self.tab3.apply.clicked.connect(self.get_current_settings)
 
     def close_and_exit(self):
         sys.exit()
@@ -421,7 +497,10 @@ class MainWindow(QWidget):
         else:
             self.statistics.setText("No device")
 
-        
+def abort_serial(device):
+    # TODO joku parempi ratkasu, nyt vaan hätäjarru
+    device.serial_.close()         
+    
 
 def set_base_settings(device):
     """
